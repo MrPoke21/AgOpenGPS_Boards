cca1e6a0eee10184ab0f53a93e581abf4d659dba
Mr. Poke <mrpoke21@gmail.com>
2026-01-21 15:20:04 +0100

    CPU idő csökkentés.

--- ESP32/ESP32_AGOPEN/src/zAutosteer.cpp
+++ ESP32/ESP32_AGOPEN/src/zAutosteer.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
-#include <zAutosteer.h>
 #include <main.h>
+#include <zAutosteer.h>
 #include <zInput.h>
-
 
 void autosteerLoop() {
 
   if (Autosteer_running) {
-    //read all the switches
-    workSwitch = digitalRead(WORKSW_PIN);  // read work switch
+    // read all the switches
+    workSwitch = digitalRead(WORKSW_PIN); // read work switch
 
-    if (steerConfig.SteerSwitch == 1)  //steer switch on - off
+    if (steerConfig.SteerSwitch == 1) // steer switch on - off
     {
-      steerSwitch = digitalRead(STEERSW_PIN);  //read auto steer enable switch open = 0n closed = Off
-    } else if (steerConfig.SteerButton == 1)   //steer Button momentary
+      steerSwitch = digitalRead(
+          STEERSW_PIN); // read auto steer enable switch open = 0n closed = Off
+    } else if (steerConfig.SteerButton == 1) // steer Button momentary
     {
       reading = digitalRead(STEERSW_PIN);
       if (reading == LOW && previous == HIGH) {
@@ -26,18 +26,21 @@ void autosteerLoop() {
         }
       }
       previous = reading;
-    } else  // No steer switch and no steer button
+    } else // No steer switch and no steer button
     {
       // So set the correct value. When guidanceStatus = 1,
       // it should be on because the button is pressed in the GUI
       // But the guidancestatus should have set it off first
-      if (guidanceStatusChanged && guidanceStatus == 1 && steerSwitch == 1 && previous == 0) {
+      if (guidanceStatusChanged && guidanceStatus == 1 && steerSwitch == 1 &&
+          previous == 0) {
         steerSwitch = 0;
         previous = 1;
       }
 
-      // This will set steerswitch off and make the above check wait until the guidanceStatus has gone to 0
-      if (guidanceStatusChanged && guidanceStatus == 0 && steerSwitch == 0 && previous == 1) {
+      // This will set steerswitch off and make the above check wait until the
+      // guidanceStatus has gone to 0
+      if (guidanceStatusChanged && guidanceStatus == 0 && steerSwitch == 0 &&
+          previous == 1) {
         steerSwitch = 1;
         previous = 0;
       }
@@ -46,42 +49,16 @@ void autosteerLoop() {
     inputHandler();
 
     switchByte = 0;
-    switchByte |= (steerSwitch << 1);  //put steerswitch status in bit 1 position
+    switchByte |= (steerSwitch << 1); // put steerswitch status in bit 1
+                                      // position
     switchByte |= workSwitch;
     calcSteerAngle();
-    if((steerEnable && motorON) || (steerEnable != motorON))
-    {
-      calcSteeringPID(); 
+    if ((steerEnable && motorON) || (steerEnable != motorON)) {
+      calcSteeringPID();
     }
-    //end of timed loop
+    // end of timed loop
   }
-  
+
   // Speed pulse
-  /**
-  if (gpsSpeedUpdateTimer < 1000)
-  {
-      if (speedPulseUpdateTimer > 200) // 100 (10hz) seems to cause tone lock ups occasionally
-      {
-          speedPulseUpdateTimer = 0;
 
-          //130 pp meter, 3.6 kmh = 1 m/sec = 130hz or gpsSpeed * 130/3.6 or gpsSpeed * 36.1111
-          //gpsSpeed = ((float)(autoSteerUdpData[5] | autoSteerUdpData[6] << 8)) * 0.1;
-          float speedPulse = gpsSpeed * 36.1111;
-
-          //Serial.print(gpsSpeed); Serial.print(" -> "); Serial.println(speedPulse);
-
-          if (gpsSpeed > 0.11) { // 0.10 wasn't high enough
-              tone(velocityPWM_Pin, uint16_t(speedPulse));
-          }
-          else {
-              noTone(velocityPWM_Pin);
-          }
-      }
-  }
-  else  // if gpsSpeedUpdateTimer hasn't update for 1000 ms, turn off speed pulse
-  {
-      noTone(velocityPWM_Pin);
-  }
-*/
-
-}  // end of main loop
+} // end of main loop
--- ESP32/ESP32_AGOPEN/src/zInput.cpp
+++ ESP32/ESP32_AGOPEN/src/zInput.cpp
@@ -1,18 +1,18 @@
-#include <zInput.h>
 #include <Configuration.h>
+#include <zInput.h>
 
-int16_t steeringPosition = 0;               //from steering sensor
-Adafruit_ADS1115 adc;  // Use this for the 16-bit version ADS1115
+int16_t steeringPosition = 0; // from steering sensor
+Adafruit_ADS1115 adc;         // Use this for the 16-bit version ADS1115
 bool adcConnected = false;
-int16_t current_zero = 0; 
+int16_t current_zero = 0;
 void initInput() {
-  //keep pulled high and drag low to activate, noise free safe
+  // keep pulled high and drag low to activate, noise free safe
   pinMode(WORKSW_PIN, INPUT_PULLUP);
   pinMode(STEERSW_PIN, INPUT_PULLUP);
   // Check ADC
   if (adc.begin()) {
     Serial.println("ADC Connecton OK");
-    adc.setDataRate(RATE_ADS1115_128SPS);  //128 samples per second
+    adc.setDataRate(RATE_ADS1115_860SPS); // 860 samples per second
     adc.setGain(GAIN_TWOTHIRDS);
     adcConnected = true;
     current_zero = adc.readADC_SingleEnded(1);
@@ -21,11 +21,10 @@ void initInput() {
   }
 }
 
-
 void inputHandler() {
-    if (!adcConnected) {
+  if (!adcConnected) {
     return;
-    }
+  }
   int sensore = adc.readADC_SingleEnded(1);
 
   // Pressure sensor?
@@ -33,7 +32,7 @@ void inputHandler() {
     sensorSample *= 0.25;
     sensorReading = sensorReading * 0.6 + sensorSample * 0.4;
     if (sensorReading >= steerConfig.PulseCountMax) {
-      steerSwitch = 1;  // reset values like it turned off
+      steerSwitch = 1; // reset values like it turned off
       currentState = 1;
       previous = 0;
     }
@@ -41,14 +40,14 @@ void inputHandler() {
 
   // Current sensor?
   if (steerConfig.CurrentSensor) {
-    sensorSample = abs((float)sensore-current_zero);
+    sensorSample = abs((float)sensore - current_zero);
 
-    sensorSample = sensorSample*CURRENT_SENSORE_MODIFIER;    
-    
+    sensorSample = sensorSample * CURRENT_SENSORE_MODIFIER;
+
     sensorReading = sensorReading * 0.7 + sensorSample * 0.3;
     sensorReading = _min(sensorReading, 255);
     if (sensorReading >= steerConfig.PulseCountMax) {
-      steerSwitch = 1;  // reset values like it turned off
+      steerSwitch = 1; // reset values like it turned off
       currentState = 1;
       previous = 0;
     }
@@ -60,19 +59,34 @@ void calcSteerAngle() {
   if (!adcConnected) {
     return;
   }
-    steeringPosition = adc.readADC_SingleEnded(0);
-  steeringPosition = (steeringPosition >> 1);  //bit shift by 2  0 to 13610 is 0 to 5v
-  helloSteerPosition = steeringPosition - 6800;
-  //convert position to steer angle. 32 counts per degree of steer pot position in my case
-  //  ***** make sure that negative steer angle makes a left turn and positive value is a right turn *****
-  if (steerConfig.InvertWAS) {
-    steeringPosition = (steeringPosition - 6805 - steerSettings.wasOffset);  // 1/2 of full scale
-    steerAngleActual = (float)(steeringPosition) / -steerSettings.steerSensorCounts;
-  } else {
-    steeringPosition = (steeringPosition - 6805 + steerSettings.wasOffset);  // 1/2 of full scale
-    steerAngleActual = (float)(steeringPosition) / steerSettings.steerSensorCounts;
+
+  int16_t sensore = adc.readADC_SingleEnded(0);
+
+  if (steeringPosition == 0 && sensore > 0) {
+    steeringPosition = sensore >> 1; // 1st time init
   }
 
-  //Ackerman fix
-  if (steerAngleActual < 0) steerAngleActual = (steerAngleActual * steerSettings.AckermanFix);
+  // EMA filter
+  // filtered = (filtered * 0.7) + (raw * 0.3) -> (filtered * 7 + raw * 3) / 10
+  steeringPosition = (steeringPosition * 7 + (sensore >> 1) * 3) / 10;
+  helloSteerPosition = steeringPosition - 6800;
+  // convert position to steer angle. 32 counts per degree of steer pot position
+  // in my case
+  //   ***** make sure that negative steer angle makes a left turn and positive
+  //   value is a right turn *****
+  if (steerConfig.InvertWAS) {
+    steeringPosition = (steeringPosition - 6805 -
+                        steerSettings.wasOffset); // 1/2 of full scale
+    steerAngleActual =
+        (float)(steeringPosition) / -steerSettings.steerSensorCounts;
+  } else {
+    steeringPosition = (steeringPosition - 6805 +
+                        steerSettings.wasOffset); // 1/2 of full scale
+    steerAngleActual =
+        (float)(steeringPosition) / steerSettings.steerSensorCounts;
+  }
+
+  // Ackerman fix
+  if (steerAngleActual < 0)
+    steerAngleActual = (steerAngleActual * steerSettings.AckermanFix);
 }
